60c7f63a7e3689607d717281d887632dc7003044
LucThi <luc.thi@dirox.net>
2023-05-01 15:42:41 +0700

    Add linting and formatter

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Head, Html, NextScript, Main } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html className="dark">
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
